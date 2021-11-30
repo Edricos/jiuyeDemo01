79eafc72b5523e0e1a3d0984045195b2e388e4c9
edric <edricgjm@qq.com>
2021-11-30 08:36:12 +0800

    11.29

--- demo01/src/main/java/com/example/demo01/controller/IndexController.java
+++ demo01/src/main/java/com/example/demo01/controller/IndexController.java
@@ -11,4 +11,9 @@ public class IndexController {
     public String index(Model model){
         return "index";
     }
+
+    @RequestMapping("/indexs")
+    public String indexs(){
+        return "indexs";
+    }
 }
