74c6b8c7faa6f80d50ebb8b5d3f4c9fdb98d482c
Edricos <88429935+Edricos@users.noreply.github.com>
2021-12-08 19:35:57 +0800

    12.08

---
+++ demo01/src/main/java/com/example/demo01/controller/TeacherController.java
@@ -0,0 +1,18 @@
+package com.example.demo01.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/tea")
+public class TeacherController {
+
+//    @RequestMapping("/info")
+//    public Teacher info(int id, HttpSession session){
+//
+//    }
+}
